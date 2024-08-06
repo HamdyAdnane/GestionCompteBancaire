d58c79ebfe44d057d481cbc02e2a8f1fa98299bd
Adnane Hamdy <a.hamdi@devaxy.com>
2024-08-06 16:36:03 +0100

    Complete La Gestion De Compte Bancaire

--- GestionCompteBancaire/CompteBancaire.cs
+++ GestionCompteBancaire/CompteBancaire.cs
@@ -9,8 +9,12 @@ namespace GestionCompteBancaire.ConsoleApp
     public class CompteBancaire
     {
         public int Id { get; set; }
-        public string Reference { get; set; }
-        public string Libelle { get; set; }
-        public string Societe { get; set; }
+        public string? Reference { get; set; }
+        public string? Libelle { get; set; }
+        public string? Societe { get; set; }
+        public override string ToString()
+        {
+            return $"[{Id}] {Reference} {Libelle} {Societe}";
+        }
     }
 }
--- GestionCompteBancaire/Program.cs
+++ GestionCompteBancaire/Program.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
-using Mehode.Crud;
 using GestionCompteBancaire.ConsoleApp;
 using ReadValeur;
-using System.IO;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Net.Sockets;
+
 
 namespace GestionCompteBancaire
 {
@@ -24,36 +26,144 @@ namespace GestionCompteBancaire
         }
         public void UserCommand()
         {
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            SqlConnection connection = new SqlConnection(configuration.GetSection("ConnectionString").Value);
             do
             {
                 int choix = ChoixMenu();
-                List<CompteBancaire> compte = new List<CompteBancaire>();
                 if (choix == 1)
                 {
-                    int[] index = new int[100];
-                    Crud.Insertion(compte, index);
-                    return;
+                    var CompteBancaireToInsert = new CompteBancaire
+                    {
+                        Reference = Read.ReadString("Entrer un Reference : "),
+                        Libelle = Read.ReadString("Entrer un Libelle : "),
+                        Societe = Read.ReadString("Entrer un Societe : ")
+                    };
+                    var insertion = "INSERT INTO CompteBancaire (Reference,Libelle,Societe) VALUES" +
+                        $"(@Reference,@Libelle,@Societe);" +
+                        $"SELECT CAST (scope_identity() AS int)";
+                    SqlParameter ReferenceParameter = new SqlParameter
+                    {
+                        ParameterName = "@Reference",
+                        SqlDbType = SqlDbType.VarChar,
+                        Direction = ParameterDirection.Input,
+                        Value = CompteBancaireToInsert.Reference
+                    };
+                    SqlParameter LibelleParameter = new SqlParameter
+                    {
+                        ParameterName = "@Libelle",
+                        SqlDbType = SqlDbType.VarChar,
+                        Direction = ParameterDirection.Input,
+                        Value = CompteBancaireToInsert.Libelle
+                    };
+                    SqlParameter SocieteParameter = new SqlParameter
+                    {
+                        ParameterName = "@Societe",
+                        SqlDbType = SqlDbType.VarChar,
+                        Direction = ParameterDirection.Input,
+                        Value = CompteBancaireToInsert.Societe
+                    };
+                    SqlCommand command = new SqlCommand(insertion, connection);
+                    command.Parameters.Add(ReferenceParameter);
+                    command.Parameters.Add(LibelleParameter);
+                    command.Parameters.Add(SocieteParameter);
+                    command.CommandType = CommandType.Text;
+                    connection.Open();
+                    CompteBancaireToInsert.Id = (int) command.ExecuteScalar();
+                    Console.WriteLine($"Compte Bancaire {CompteBancaireToInsert.Id} ajouté avec succès ");
+                    connection.Close();
                 }
                 else if (choix == 2)
                 {
-                    var affichage = Crud.Affichage(ListData.GetCompteBancaire());
-                    foreach (var affiche in affichage)
-                        Console.WriteLine("ID :" + affiche.Id + " Reference :" + affiche.Reference + " Libelle :" + affiche.Libelle + " Societe :" + affiche.Societe);
-                    return;
+                    var affichage = "SELECT * FROM CompteBancaire";
+                    SqlCommand command = new SqlCommand(affichage, connection);
+                    command.CommandType = CommandType.Text;
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    CompteBancaire compteBancaire;
+                    while (reader.Read())
+                    {
+                        compteBancaire = new CompteBancaire
+                        {
+                            Id = reader.GetInt32("Id"),
+                            Reference = reader.GetString("Reference"),
+                            Libelle = reader.GetString("Libelle"),
+                            Societe = reader.GetString("Societe")
+                        };
+                        Console.WriteLine(compteBancaire);
+                    }
+                    connection.Close();
                 }
                 else if (choix == 3)
                 {
-                    var supprimer = Crud.Supprimer(ListData.GetCompteBancaire());
-                    foreach (var affiche in supprimer)
-                        Console.WriteLine("ID :" + affiche.Id + " Reference :" + affiche.Reference + " Libelle :" + affiche.Libelle + " Societe :" + affiche.Societe);
-                    return;
+                    var CompteBancaireToDelete = new CompteBancaire
+                    {
+                        Id = Read.ReadInt("Entre L'ID Pour Delete :")
+                    };
+                    var supprimer = "DELETE FROM CompteBancaire WHERE Id = @Id";
+                    SqlParameter IdParameter = new SqlParameter
+                    {
+                        ParameterName = "@Id",
+                        SqlDbType = SqlDbType.Int,
+                        Direction = ParameterDirection.Input,
+                        Value = CompteBancaireToDelete.Id
+                    };
+                    SqlCommand command = new SqlCommand(supprimer, connection);
+                    command.Parameters.Add(IdParameter);
+                    command.CommandType = CommandType.Text;
+                    connection.Open();
+                    if (command.ExecuteNonQuery() > 0)
+                        Console.WriteLine($"Compte bancaire Supprimé avec succès");
+                    connection.Close();
                 }
                 else if (choix == 4)
                 {
-                    var modifier = Crud.Modification(ListData.GetCompteBancaire());
-                    foreach (var affiche in modifier)
-                        Console.WriteLine("ID :" + affiche.Id + " Reference :" + affiche.Reference + " Libelle :" + affiche.Libelle + " Societe :" + affiche.Societe);
-                    return;
+                    var CompteBancaireToUpdate = new CompteBancaire
+                    {
+                        Id = Read.ReadInt("Donnez-moi l'ID dont vous modifiez les propriétés : "),
+                        Reference = Read.ReadString("Entrer Nouveau Reference : "),
+                        Libelle = Read.ReadString("Entrer Nouveau Libelle : "),
+                        Societe = Read.ReadString("Entrer Nouveau Societe : ")
+                    };
+                    var modification = "UPDATE CompteBancaire SET Reference = @Reference, Libelle = @Libelle ,Societe = @Societe WHERE Id = @Id";
+                    SqlParameter IdParameter = new SqlParameter
+                    {
+                        ParameterName = "@Id",
+                        SqlDbType = SqlDbType.Int,
+                        Direction = ParameterDirection.Input,
+                        Value = CompteBancaireToUpdate.Id
+                    };
+                    SqlParameter ReferenceParameter = new SqlParameter
+                    {
+                        ParameterName = "@Reference",
+                        SqlDbType = SqlDbType.VarChar,
+                        Direction = ParameterDirection.Input,
+                        Value = CompteBancaireToUpdate.Reference
+                    };
+                    SqlParameter LibelleParameter = new SqlParameter
+                    {
+                        ParameterName = "@Libelle",
+                        SqlDbType = SqlDbType.VarChar,
+                        Direction = ParameterDirection.Input,
+                        Value = CompteBancaireToUpdate.Libelle
+                    };
+                    SqlParameter SocieteParameter = new SqlParameter
+                    {
+                        ParameterName = "@Societe",
+                        SqlDbType = SqlDbType.VarChar,
+                        Direction = ParameterDirection.Input,
+                        Value = CompteBancaireToUpdate.Societe
+                    };
+                    SqlCommand command = new SqlCommand(modification, connection);
+                    command.Parameters.Add(IdParameter);
+                    command.Parameters.Add(ReferenceParameter);
+                    command.Parameters.Add(LibelleParameter);
+                    command.Parameters.Add(SocieteParameter);
+                    command.CommandType = CommandType.Text;
+                    connection.Open();
+                    if (command.ExecuteNonQuery() > 0)
+                        Console.WriteLine($"Compte bancaire mis à jour avec succès.");
+                    connection.Close();
                 }
                 else if (choix == 5)
                 {
